a7e3125fe9fa75f0f9c0e99215d8bcd16b3d5676
mcudny <166936960+mcudny@users.noreply.github.com>
2024-06-15 10:17:53 +0200

    Main.java

--- Zadanie 11/src/Main.java
+++ Zadanie 11/src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.Scanner;
+public  class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int a;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int suma = 0;
+        int ilosc = 0;
+        System.out.println("Wprowadz liczby całkowite. Aby zakończyć wpisz 0");
+        while(true){
+            System.out.print("Podaj liczby całkowite: ");
+            while (!scanner.hasNextInt()){
+                System.out.println("To nie jest liczba całkowita! Spróbuj ponownie.");
+                scanner.next();
+                System.out.print("Podaj liczby całkowite: ");
+            }
+            a = scanner.nextInt();
+            if(a ==0){
+                break;
+            }
+            if (a<min){
+                min = a;
+            }
+            if (a>max){
+                max = a;
+            }
+            suma += a;
+            ilosc++;
         }
+        double srednia = (double)suma/ilosc;
+        System.out.println("Suma liczb wynosi: "+suma);
+        System.out.printf("Wartosc arytmetyczna wynosi: %.2f",srednia);
     }
 }
